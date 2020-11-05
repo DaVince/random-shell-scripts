cc55775a2b4842fc8a228377b1fd1a5fb69cf2b1
Vincent Beers <vincentbeers@gmail.com>
2020-11-05 17:39:37 +0100

    Rename bulk conversion scripts

--- bulk-mptm-to-wav.sh
+++ bulk-mptm-to-wav.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # convert MPTM music (or other module tracker files) into WAV files with openmpt123
 
-for i in `ls *.mptm *.it`; do
-  openmpt123 "${i}" -o "out/${i}.wav"
+for i in `ls *.mptm *.it *.mod *.xm *.s3m`; do
+  openmpt123 "${i}" -o "${i}.wav"
 done
