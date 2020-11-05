47febce8ed1474a953189e130e9c77ef555333e1
Vincent Beers <vincentbeers@gmail.com>
2020-11-05 17:21:42 +0100

    Add consistency to code style

--- make-video-from-wav.sh
+++ make-video-from-wav.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Read wav file and turn into MP4, displaying cover.png as the video frame
 
-for file in `ls *.wav`
-do
-  ffmpeg -loop 1 -i cover.png -i "${file}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "video-${file}.mp4"
+for i in ./*.wav; do
+  ffmpeg -loop 1 -i cover.png -i "${i}" -c:v libx264 -tune stillimage -c:a aac -b:a 192k -pix_fmt yuv420p -shortest "video-${i}.mp4"
 done
